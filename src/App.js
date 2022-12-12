6a5fd9c778630fc5e0b392bcfafb00dd03d32110
Erichards01 <erichards066@gmail.com>
2022-12-12 17:03:41 +0800

    change background to red

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import About from "./components/about";
 
 function App() {
   return (
-    <main>
-      <About
-        headingOne="Hi, I'm Elliott Richards"
-        headingTwo="A passionate coder!"
-        hello=" I've enjoyed every bit of learning frontend development over my
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <main>
+        <About
+          headingOne="Hi, I'm Elliott Richards"
+          headingTwo="A passionate coder!"
+          hello=" I've enjoyed every bit of learning frontend development over my
         journey. I love the challenge and the endless learning."
-        interests="  I have a dog called Stanley that keeps me busy. I love AFL, I'm a big
+          interests="  I have a dog called Stanley that keeps me busy. I love AFL, I'm a big
         Fremantle Dockers fan. I love catching up with friends and
         family."
-        contact="Contact details below- "
-        email=" Email: e.richards066@gmail.com "
-        linkedin="https://www.linkedin.com/in/elliott-richards01"
-      />
-    </main>
+          contact="Contact details below- "
+          email=" Email: e.richards066@gmail.com "
+          linkedin="https://www.linkedin.com/in/elliott-richards01"
+        />
+      </main>{" "}
+    </meta>
   );
 }
 
