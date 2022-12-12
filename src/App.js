a76083d1e64ffe7cd256de3deb0f16ef21e38804
Erichards01 <erichards066@gmail.com>
2022-12-12 17:15:39 +0800

    media query added screen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import "./App.css";
 import React from "react";
-import { Helmet } from "react-helmet";
 
 import About from "./components/about";
 
 function App() {
   return (
-    <React.Fragment>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Elliott-Richards</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
-      <main>
-        <About
-          headingOne="Hi, I'm Elliott Richards"
-          headingTwo="A passionate coder!"
-          hello=" I've enjoyed every bit of learning frontend development over my
+    <main>
+      <About
+        headingOne="Hi, I'm Elliott Richards"
+        headingTwo="A passionate coder!"
+        hello=" I've enjoyed every bit of learning frontend development over my
         journey. I love the challenge and the endless learning."
-          interests="  I have a dog called Stanley that keeps me busy. I love AFL, I'm a big
+        interests="  I have a dog called Stanley that keeps me busy. I love AFL, I'm a big
         Fremantle Dockers fan. I love catching up with friends and
         family."
-          contact="Contact details below- "
-          email=" Email: e.richards066@gmail.com "
-          linkedin="https://www.linkedin.com/in/elliott-richards01"
-        />
-      </main>
-    </React.Fragment>
+        contact="Contact details below- "
+        email=" Email: e.richards066@gmail.com "
+        linkedin="https://www.linkedin.com/in/elliott-richards01"
+      />
+    </main>
   );
 }
 
